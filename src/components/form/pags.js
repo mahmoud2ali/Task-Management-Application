cc00066563c2f029e2d75475cbd0bf0a05a7d418
Mahmoud <mahmoud2m2ali@gmail.com>
2024-09-10 03:11:00 +0300

    media query added.

--- src/components/form/pags.js
+++ src/components/form/pags.js
@@ -27,10 +27,11 @@ export default function Form() {
     return (
         <div>
             <div className={styles.form}>
-                <h1>Wellcome to TODO. </h1>
+                <h1><span className = {styles.t}>t</span><span className ={styles.o1}>o</span><span className = {styles.d}>d</span><span className = {styles.o2}>o</span></h1>
+                <h2>Add a new task</h2>
                 <input type="text" value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)} />
                 <input type="text" value={content} placeholder="content" onChange={(e) => setContent(e.target.value)} />
-                <button type="button" onClick={save}>Save</button>
+                <button type="button" onClick={save}>Add</button>
             </div>
             <div className={styles.tasks}>
                 {tasks.map((task, index) => (
--- src/components/task/page.js
+++ src/components/task/page.js
@@ -2,16 +2,19 @@ import styles from "./page.module.css"
 
 
 function Task({ title, content, onDelete }) {
-  
+
 
 
     return (
         <div className={styles.main}>
             <div>
-                <h2>{title}</h2>
+                <div className={styles.top}>
+                    <h2>{title}</h2>
+                    <button onClick={onDelete}>delete</button>
+                </div>
                 <p>{content}</p>
             </div>
-            <button onClick={onDelete}>delete</button>
+
         </div>
     )
 }
